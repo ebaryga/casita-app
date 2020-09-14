69f71cb3613d128d40e9842edf0faf2cf95f21e3
Henri Fardeau <henri.fardeau@outlook.fr>
2020-09-14 10:48:19 +0200

    fix: tailwind postcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const HomePage = () => <div>Hello world!</div>;
+const HomePage = () => (
+  <div>
+    <h1 className="text-blue-100">Hello world!</h1>
+  </div>
+);
 
 export default HomePage;
