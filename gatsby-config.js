e5de627371b73ad7b54187d1191a0b6ae8d35d94
Henri Fardeau <henri.fardeau@outlook.fr>
2020-09-14 10:36:31 +0200

    chore: inital commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,14 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        printRejected: false,
+        develop: false,
+        tailwind: true,
+        // whitelistPatterns: [/^slick-/],
+      },
+    },
+  ],
+};
